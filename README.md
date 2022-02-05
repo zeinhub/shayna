188d4b29a9bcf4923162135cc175df04703e3dac
Awiez Fathwa Zein <awiezfathwa@gmail.com>
2022-02-05 20:21:12 +0700

    upload project vue

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # shayna
-Project aplikasi vue sebagai bahan belajar kelas full stack laravel vue js (toko online)
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <HeaderShayna/>
+    <HeroSection/>
+    <WomenBanner/>
+    <InstaShayna/>
+    <PartnerShayna/>
+    <FooterShayna/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroSection from '@/components/HeroSection.vue'
+import WomenBanner from '@/components/WomenBanner'
+import InstaShayna from '@/components/InstaShayna'
+import PartnerShayna from '@/components/PartnerShayna'
+import FooterShayna from '@/components/FooterShayna'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+    HeaderShayna,
+    HeroSection,
+    WomenBanner,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+  }
+}
+</script>
